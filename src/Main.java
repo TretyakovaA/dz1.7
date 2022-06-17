ad44edfe4addd130e81c9c40a7adead1cf8646c7
TretyakovaA <putinvivo@bk.ru>
2022-06-17 20:08:27 +0300

    draft in progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phone = "59604157537";
+        if (phone.length() ==10) {
+            phone = "+7" + phone;
+        } else if (phone.length() >11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length()<10) {
+            throw  new RuntimeException(" Телефон слишком короткий");
+            
+        } else if ((phone.length() == 11) && (phone.charAt(0) != '7')) {
+            throw new RuntimeException("Среди нас посторонний");
+        }
+        System.out.println("phone = " + phone);
+
+
     }
 }
